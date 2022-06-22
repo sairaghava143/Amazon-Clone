c52fb43d5b7573b904552d6bee5e6ac2f4f91950
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-06-22 19:32:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Payment from "./components/Payment/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./components/Orders/Orders";
+
+const promise = loadStripe(
+  "pk_test_51HPvesLv2wbjCv6jorIW0Jwaj6EZNvoCM9f1MiXoWxeqKNAzQMNzkz4sKMJ7FAfm4BTqg6RjQuJCsDCjJzxDSDVT00hE9xbBLW"
+);
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("User >>>", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    //BEM Convention
+
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login"></Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useStateValue } from "../../StateProvider";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
+import FlipMove from "react-flip-move";
+function Checkout() {
+  const [{ basket, user }, disptch] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Books/062020/Bookbazaar/Header_showcases._CB407576112_SY500_.jpg"
+          alt=""
+        />
+
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your shopping basket is empty</h2>
+            <p>
+              You have no items in your cat. To buy one, click on "Add to Cart"
+              next to the item
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h3>Hello, {user ? user.email : "Guest"}</h3>
+            <h2 className="checkout__title">Your shopping Basket</h2>
+            <FlipMove>
+              {basket.map((item) => (
+                <CheckoutProduct
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              ))}
+            </FlipMove>
+            {/* CheckoutProduct */}
+          </div>
+        )}
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
